5c9d1d402d9c0af0149071966cbafbc7226aef10
nicksonmogaka <nickson.mogaka@student.moringaschool.com>
2024-09-01 12:51:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,46 @@
+def add_numbers(num1, num2):
+    return num1 + num2
 
+def is_even(number):
+    return number % 2 == 0
+
+def reverse_string(text):
+    return text[::-1]
+
+def count_vowels(text):
+    vowels = 'aeiou'
+    return sum(1 for char in text.lower() if char in vowels)
+
+def calculate_factorial(n):
+    if n < 0:
+        raise ValueError("Input must be a non-negative integer.")
+    elif n == 0:
+        return 1
+    else:
+        return n * calculate_factorial(n-1)
+
+def decorator_func(func):
+    def wrapper(*args, **kwargs):
+        print("Decorator Applied")
+        return func(*args, **kwargs)
+    return wrapper
+
+def sort_by_age(people):
+    return sorted(people, key=lambda x: x[1])
+
+def merge_dicts(dict1, dict2):
+    merged_dict = dict1.copy()
+    for key, value in dict2.items():
+        merged_dict[key] = merged_dict.get(key, 0) + value
+    return merged_dict
+
+class Car:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+
+    def display_info(self):
+        print(f"Make: {self.make}")
+        print(f"Model: {self.model}")
+        print(f"Year: {self.year}")
